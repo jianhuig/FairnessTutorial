1d8958aaaf6c898fe727413e237c968979baead6
jianhuig <jianhui.gao@mail.utoronto.ca>
2024-02-04 16:47:53 -0500

    test positive class balance

--- R/Fairness.R
+++ R/Fairness.R
@@ -957,64 +957,89 @@ eval_treatment_equality <- function(data, outcome, group, probs, cutoff = 0.5,
 #' Examine balance for positive class of a model
 #' @param data Data frame containing the outcome, predicted outcome, and
 #' sensitive attribute
-#' @param outcome the name of the outcome variable, it must be binary
-#' @param group the name of the sensitive attribute
-#' @param probs the name of the predicted outcome variable
-#' @param cutoff the threshold for the predicted outcome, default is 0.5
-#' @param confint whether to compute 95% confidence interval, default is TRUE
-#' @param bootstraps the number of bootstrap samples, default is 1000
-#' @return a list of expected positive score, the difference, and their confidence interval
-#' @importFrom magrittr %>%
+#' @param outcome Name of the outcome variable
+#' @param group Name of the sensitive attribute
+#' @param probs Predicted probabilities
+#' @param cutoff Cutoff value for the predicted probabilities
+#' @param confint Logical indicating whether to calculate confidence intervals
+#' @param bootstraps Number of bootstraps to use for confidence intervals
+#' @param digits Number of digits to round the results to, default is 2
+#' @param message Whether to print the results, default is TRUE
+#' @return A list containing the following elements:
+#' - Average predicted probability for Group 1
+#' - Average predicted probability for Group 2
+#' - Difference in average predicted probability
+#' If confidence intervals are computed (`confint = TRUE`):
+#' - 95% CI for the difference in average predicted probability
 #' @export
 
-balance_positive <- function(data, outcome, group, probs, cutoff = 0.5,
-                             confint = TRUE, bootstraps = 1000) {
+eval_pos_class_bal <- function(data, outcome, group, probs, cutoff = 0.5,
+                             confint = TRUE, bootstraps = 1000,
+                             digits = 2, message = TRUE) {
   # Check if outcome is binary
   unique_values <- unique(data[[outcome]])
   if (!(length(unique_values) == 2 && all(unique_values %in% c(0, 1)))) {
     stop("Outcome must be binary (containing only 0 and 1).")
   }
 
-  expected_positive_score <- get_exp_pos(
-    data = data, outcome = outcome, group = group, probs = probs,
-    cutoff = cutoff
+  pos_data <- data[data[[outcome]] == 1, ]
+  avg_prob <- get_avg_prob(
+    data = pos_data, group = group, probs = probs
   )
 
-  # Calculate confidence interval
-  if (confint) {
-    expected_positive_score_se <- lapply(1:bootstraps, function(j) {
-      data_boot <- data[sample(nrow(data), replace = TRUE), ]
-      get_exp_pos(
-        data = data_boot, outcome = outcome, group = group,
-        probs = probs, cutoff = cutoff
-      )
-    }) %>%
-      do.call(rbind, .) %>%
-      dplyr::group_by(!!rlang::sym(group)) %>%
-      dplyr::summarize_all(function(x) stats::sd(logit(x))) %>%
-      dplyr::rename(expected_positive_score_se = exp_pos, expected_positive_score_diff_se = expected_positive_diff)
+  avg_prob$Avg_Prob_Diff <- avg_prob[[1]] - avg_prob[[2]]
 
-    expected_positive_score <- expected_positive_score %>%
-      dplyr::left_join(expected_positive_score_se, by = group) %>%
-      dplyr::mutate(
-        expected_positive_score_lower = expit(logit(exp_pos) - 1.96 * expected_positive_score_se),
-        expected_positive_score_upper = expit(logit(exp_pos) + 1.96 * expected_positive_score_se),
-        expected_positive_score_diff_lower = expit(logit(expected_positive_diff) - 1.96 * expected_positive_score_diff_se),
-        expected_positive_score_diff_upper = expit(logit(expected_positive_diff) + 1.96 * expected_positive_score_diff_se)
-      ) %>%
-      dplyr::select(
-        !!rlang::sym(group), exp_pos, expected_positive_score_lower, expected_positive_score_upper,
-        expected_positive_diff, expected_positive_score_diff_lower, expected_positive_score_diff_upper
-      ) %>%
-      dplyr::mutate(
-        expected_positive_score_ci = paste0("[", round(expected_positive_score_lower, 3), ", ", round(expected_positive_score_upper, 3), "]"),
-        expected_positive_score_diff_ci = paste0("[", round(expected_positive_score_diff_lower, 3), ", ", round(expected_positive_score_diff_upper, 3), "]")
-      ) %>%
-      dplyr::select(-expected_positive_score_lower, -expected_positive_score_upper, -expected_positive_score_diff_lower, -expected_positive_score_diff_upper)
-    expected_positive_score[, c(1, 2, 4, 3, 5)]
-  } else {
-    return(expected_positive_score)
+  if (confint) {
+    se <- lapply(1:bootstraps, function(j) {
+      # bootstrap within each group
+      group1 <- sample(which(data[[group]] == unique(data[[group]])[1]),
+                       replace = TRUE
+      )
+      group2 <- sample(which(data[[group]] == unique(data[[group]])[2]),
+                       replace = TRUE
+      )
+      data_boot <- rbind(data[group1, ], data[group2, ])
+      pos_data_boot <- data_boot[data_boot[[outcome]] == 1, ]
+      avg_prob_boot <- get_avg_prob(
+        data = pos_data_boot, group = group, probs = probs
+      )
+      avg_prob_boot[[1]] - avg_prob_boot[[2]]
+    })
+    avg_prob$"Avg_Prob_Diff_CI" <- c(
+      avg_prob$"Avg_Prob_Diff" - 1.96 * sd(unlist(se)),
+      avg_prob$"Avg_Prob_Diff" + 1.96 * sd(unlist(se))
+    )
   }
+
+  if (message) {
+    cat(
+      "Average predicted probability for Group", unique(data[[group]])[1], "is",
+      round(avg_prob[[1]], digits), "\n"
+    )
+    cat(
+      "Average predicted probability for Group", unique(data[[group]])[2], "is",
+      round(avg_prob[[2]], digits), "\n"
+    )
+    cat("Difference in average predicted probability is",
+        round(avg_prob$"Avg_Prob_Diff", digits), "\n")
+    if (confint) {
+      cat(
+        "95% CI for the difference in average predicted probability is",
+        round(avg_prob$"Avg_Prob_Diff_CI"[1], digits), "to",
+        round(avg_prob$"Avg_Prob_Diff_CI"[2], digits), "\n"
+      )
+      if (avg_prob$"Avg_Prob_Diff_CI"[1] > 0 |
+          avg_prob$"Avg_Prob_Diff_CI"[2] < 0) {
+        cat("There is enough evidence that the model does not satisfy
+            treatment equality.\n")
+      } else {
+        cat("There is not enough evidence that the model does not satisfy
+            treatment equality.\n")
+      }
+    }
+  }
+
+  return(avg_prob)
 }
 
 #' Examine balance for negative class of a model
--- R/ModelPerformance.R
+++ R/ModelPerformance.R
@@ -183,56 +183,22 @@ get_err_ratio <- function(data, outcome, group, probs, cutoff = 0.5) {
   return(err_ratio)
 }
 
-#' Calculate the expected positive score
+#' Calculate the average predicted probability
 #' @param data Data frame containing the outcome, predicted outcome, and
 #' sensitive attribute
 #' @param outcome the name of the outcome variable, it must be binary
 #' @param group the name of the sensitive attribute
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
-#' @return a vector of expected positive score and the difference
-#' @importFrom magrittr %>%
+#' @return a list of average predicted probability
 #' @noRd
 
-get_exp_pos <- function(data, outcome, group, probs, cutoff = 0.5) {
-  # Convert strings to symbols if necessary
-  outcome_sym <- rlang::sym(outcome)
-  group_sym <- rlang::sym(group)
-  probs_sym <- rlang::sym(probs)
-
-  # Calculate expected positive score
-  result <- data %>%
-    dplyr::filter(!!outcome_sym == 1) %>%
-    dplyr::group_by(!!group_sym) %>%
-    dplyr::summarize(exp_pos = mean(!!probs_sym), .groups = "drop") %>%
-    dplyr::mutate(expected_positive_diff = abs(diff(exp_pos)))
-
-  return(result)
-}
-
-#' Calculate the expected negative score
-#' @param data Data frame containing the outcome, predicted outcome, and
-#' sensitive attribute
-#' @param outcome the name of the outcome variable, it must be binary
-#' @param group the name of the sensitive attribute
-#' @param probs the name of the predicted outcome variable
-#' @param cutoff the threshold for the predicted outcome, default is 0.5
-#' @return a vector of expected negative score and the difference
-#' @importFrom magrittr %>%
-#' @noRd
-
-get_exp_neg <- function(data, outcome, group, probs, cutoff = 0.5) {
-  # Convert strings to symbols if necessary
-  outcome_sym <- rlang::sym(outcome)
-  group_sym <- rlang::sym(group)
-  probs_sym <- rlang::sym(probs)
-
-  # Calculate expected negative score
-  result <- data %>%
-    dplyr::filter(!!outcome_sym == 0) %>%
-    dplyr::group_by(!!group_sym) %>%
-    dplyr::summarize(exp_neg = mean(!!probs_sym), .groups = "drop") %>%
-    dplyr::mutate(expeced_negative_diff = abs(diff(exp_neg)))
-
-  return(result)
+get_avg_prob <- function(data, outcome, group, probs, cutoff = 0.5) {
+  avg_prob <- list()
+  for (i in unique(data[, group])) {
+    sub_data <- data[data[, group] == i, ]
+    avg_prob[[paste0("Avg_Predicted_Prob_", i)]] <-
+      mean(sub_data[, probs])
+  }
+  return(avg_prob)
 }
