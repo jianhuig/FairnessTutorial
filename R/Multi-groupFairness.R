fe824cf5238c9e4ae27ecb91865ea5be5784270b
Benson-chou <benson.chou@mail.utoronto.ca>
2024-02-27 22:32:12 -0500

    Added most multi-group metrics

--- R/Multi-groupFairness.R
+++ R/Multi-groupFairness.R
@@ -1,4 +1,6 @@
 # Load data
+library(PheCAP)
+data(ehr_data)
 data <- PheCAP::PhecapData(ehr_data, "healthcare_utilization", "label", 0.4)$frame
 # Binarize outcome (main_ICD)
 data <- data %>% mutate(outcome = ifelse(main_ICD > 0, 1, 0))
@@ -26,7 +28,7 @@ y_test <- test %>% select(outcome)
 
 # Manually create 10-folds to make sure the results are reproducible
 set.seed(30)
-folds <- createFolds(y_train, k = 10, list = TRUE)
+folds <- caret::createFolds(y_train, k = 10, list = TRUE)
 
 # Convert the list to a vector where each element indicates the fold number
 fold_ids <- rep(NA, length(y_train))
@@ -36,7 +38,7 @@ for(i in 1:10) {
 
 # To choose lambda (regularization parameter), cross-validation is typically used
 # Pass the fold_ids to cv.glmnet through the foldid argument
-cv_fit <- cv.glmnet(x_train, y_train, family = "binomial", alpha = 1, foldid = fold_ids)
+cv_fit <- glmnet::cv.glmnet(x_train, y_train, family = "binomial", alpha = 1, foldid = fold_ids)
 
 # Best lambda value
 best_lambda <- cv_fit$lambda.min
@@ -45,23 +47,50 @@ best_lambda <- cv_fit$lambda.min
 fit <- glmnet::glmnet(x_train, y_train, family = "binomial", alpha=1, lambda = best_lambda)
 
 # Predictions
-test$pred <- predict(fit, newx = x_test, type = "response", s = best_lambda)[,1]
-mean(ifelse(test$pred > 0.4, 1, 0) == y_test)
+cut_off <- 0.4
+test$prob <- predict(fit, newx = x_test, type = "response", s = best_lambda)[,1]
+mean(ifelse(test$prob > cut_off, 1, 0) == y_test)
 
 # The dataframe would need predictions, outcome, HU_group
-test %>%
-  mutate(pred = ifelse(pred > 0.4, 1, 0)) %>%
-  filter(outcome == 0) %>%
-  summarise(fpr = mean(pred))
+test <- test %>%
+  mutate(pred = ifelse(prob > cut_off, 1, 0))
 
 # Calculate the fairness metrics
 test <- test %>%
   mutate(hu_group = ifelse(HU_group == 1, paste0("0 to ", quantile(data$healthcare_utilization, 0.25)),
                            ifelse(HU_group ==2, paste0(quantile(data$healthcare_utilization, 0.25), " to ", quantile(data$healthcare_utilization, 0.5)),
                                   ifelse(HU_group == 3, paste0(quantile(data$healthcare_utilization, 0.5), " to ", quantile(data$healthcare_utilization, 0.75)), paste0(quantile(data$healthcare_utilization, 0.75), " to ", quantile(data$healthcare_utilization, 1))))))
-cut_off <- 0.4
-# eq_oppt <- eval_eq_opp(dat = test,
-#                                          outcome = "outcome",
-#                                          group = "hu_group",
-#                                          probs = "pred",
-#                                          cutoff = cut_off)
+
+
+# Calculate the Max Min Difference
+max_min_diff <- test %>% dplyr::group_by(hu_group) %>%
+  summarise(max_min_diff = max(test$prob) - min(test$prob))
+
+max_min_diff
+
+# Calculate the Max-Min Ratio
+max_min_ratio <- test %>% dplyr::group_by(hu_group) %>%
+  summarise(max_min_ratio = max(test$prob)/min(test$prob))
+
+max_min_ratio
+
+# Calculate the Max Absolute Difference
+mean_ab_diff <- test %>% dplyr::group_by(hu_group) %>%
+  summarise(mean_ab_diff = abs(max(test$prob) - mean(test$prob)))
+
+mean_ab_diff
+
+# Calculate the Mean Absolute Deviance (MAD)
+mad <- test %>% dplyr::group_by(hu_group) %>%
+  summarise(MAD = mad(test$prob, center=mean(test$prob)))
+
+mad
+
+# Calculate the variance
+variance <- test %>% dplyr::group_by(hu_group) %>%
+  summarise(MSE = mean((test$pred - mean(test$outcome))^2))
+
+variance
+
+# generalized entropy index
+
