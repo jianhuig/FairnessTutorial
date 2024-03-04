e82522ae632bb9d40403c145bd6c532a04a76e37
jianhuig <jianhui.gao@mail.utoronto.ca>
2024-03-03 22:56:05 -0500

    Update ModelPerformance.R
    add a function to calculate all metrics at once;
    sort group names;
    return a vector instead for each metric

--- R/ModelPerformance.R
+++ R/ModelPerformance.R
@@ -10,20 +10,22 @@
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of true positive rates
+#' @return a vector of true positive rates
 #' @noRd
 
 get_tpr <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
-  tpr <- list()
-  for (i in unique(data[, group])) {
-    tp <- sum(data[, outcome] == 1 &
-      data[, group] == i &
-      data[, probs] >= cutoff)
+  tpr <- c()  # Initialize empty vector for true positive rates
+  groups <- sort(unique(data[, group]))  # Get sorted unique groups
+
+  for (i in groups) {
+    tp <- sum(data[, outcome] == 1 & data[, group] == i & data[, probs] >= cutoff)
     p <- sum(data[, outcome] == 1 & data[, group] == i)
-    tpr[[paste0("TPR_", i)]] <- round(tp / p, digits)
+    tpr <- c(tpr, round(tp / p, digits))  # Calculate TPR and add to vector
   }
+
   return(tpr)
 }
+
 
 #' Calculate the false positive rate
 #' @param data Data frame containing the outcome, predicted outcome, and
@@ -33,17 +35,18 @@ get_tpr <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of false positive rates
+#' @return a vector of false positive rates
 #' @noRd
 
 get_fpr <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
-  fpr <- list()
-  for (i in unique(data[, group])) {
+  fpr <- c()
+  groups <- sort(unique(data[, group]))
+  for (i in groups) {
     fp <- sum(data[, outcome] == 0 &
       data[, group] == i &
       data[, probs] >= cutoff)
     p <- sum(data[, outcome] == 1 & data[, group] == i)
-    fpr[[paste0("FPR_", i)]] <- round(fp / p, digits)
+    fpr <- c(fpr, round(fp / p, digits))
   }
   return(fpr)
 }
@@ -56,18 +59,18 @@ get_fpr <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of probability of positive prediction
+#' @return a vector of probability of positive prediction
 #' @noRd
 
 get_ppr <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
-  ppr <- list()
+  ppr <- c()
   for (i in unique(data[, group])) {
     pp <- sum(
       data[, group] == i &
         data[, probs] >= cutoff
     )
     n <- sum(data[, group] == i)
-    ppr[[paste0("PPR_", i)]] <- round(pp / n, digits)
+    ppr <- c(ppr, round(pp / n, digits))
   }
   return(ppr)
 }
@@ -80,18 +83,18 @@ get_ppr <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of positive predictive value
+#' @return a vector of positive predictive value
 #' @noRd
 
 get_ppv <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
-  ppv <- list()
+  ppv <- c()
   for (i in unique(data[, group])) {
     tp <- sum(data[, outcome] == 1 &
       data[, group] == i &
       data[, probs] >= cutoff)
     pp <- sum(data[, group] == i &
       data[, probs] >= cutoff)
-    ppv[[paste0("PPV_", i)]] <- round(tp / pp, digits)
+    ppv <- c(ppv, round(tp / pp, digits))
   }
   return(ppv)
 }
@@ -104,18 +107,18 @@ get_ppv <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
 #' @param digits the number of digits to round the result to, default is 2
-#' @return  a list of negative predictive value
+#' @return  a vector of negative predictive value
 #' @noRd
 
 get_npv <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
-  npv <- list()
+  npv <- c()
   for (i in unique(data[, group])) {
     tn <- sum(data[, outcome] == 0 &
       data[, group] == i &
       data[, probs] < cutoff)
     nn <- sum(data[, group] == i &
       data[, probs] < cutoff)
-    npv[[paste0("NPV_", i)]] <- round(tn / nn, digits)
+    npv <- c(npv, round(tn / nn, digits))
   }
   return(npv)
 }
@@ -128,11 +131,11 @@ get_npv <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of accuracy
+#' @return a vector of accuracy
 #' @noRd
 
 get_acc <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
-  acc <- list()
+  acc <- c()
   for (i in unique(data[, group])) {
     tp <- sum(data[, outcome] == 1 &
       data[, group] == i &
@@ -141,7 +144,7 @@ get_acc <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
       data[, group] == i &
       data[, probs] < cutoff)
     p <- sum(data[, group] == i)
-    acc[[paste0("ACC_", i)]] <- round((tp + tn) / p, digits)
+    acc <- c(acc, round((tp + tn) / p, digits))
   }
   return(acc)
 }
@@ -153,20 +156,19 @@ get_acc <- function(data, outcome, group, probs, cutoff = 0.5, digits = 2) {
 #' @param group the name of the sensitive attribute
 #' @param probs the name of the predicted outcome variable
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of brier score
+#' @return a vector of brier score
 #' @noRd
 
 get_brier_score <- function(data, outcome, group, probs, digits = 2) {
-  brier_score <- list()
+  brier_score <- c()
   for (i in unique(data[, group])) {
     sub_data <- data[data[, group] == i, ]
-    brier_score[[paste0("Brier_", i)]] <-
-      round(mean((sub_data[, outcome] - sub_data[, probs])^2), digits)
+    brier_score <- c(brier_score, round(mean((sub_data[, outcome] - sub_data[, probs])^2), digits))
   }
   return(brier_score)
 }
 
-#' Calculate the the raitio of false negative to false positive
+#' Calculate the the ratio of false negative to false positive
 #' @param data Data frame containing the outcome, predicted outcome, and
 #' sensitive attribute
 #' @param outcome the name of the outcome variable, it must be binary
@@ -174,19 +176,19 @@ get_brier_score <- function(data, outcome, group, probs, digits = 2) {
 #' @param probs the name of the predicted outcome variable
 #' @param cutoff the threshold for the predicted outcome, default is 0.5
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of the ratio of false negative to false positive
+#' @return a vector of the ratio of false negative to false positive
 #' @noRd
 
 get_err_ratio <- function(data, outcome, group, probs, cutoff = 0.5,
                           digits = 2) {
-  err_ratio <- list()
+  err_ratio <- c()
   for (i in unique(data[, group])) {
     sub_data <- data[data[, group] == i, ]
     fp <- sum(sub_data[, outcome] == 0 &
       sub_data[, probs] >= cutoff)
     fn <- sum(sub_data[, outcome] == 1 &
       sub_data[, probs] < cutoff)
-    err_ratio[[paste0("FN/FP_", i)]] <- round(fn / fp, digits)
+    err_ratio <- c(err_ratio, round(fn / fp, digits))
   }
   return(err_ratio)
 }
@@ -198,15 +200,52 @@ get_err_ratio <- function(data, outcome, group, probs, cutoff = 0.5,
 #' @param group the name of the sensitive attribute
 #' @param probs the name of the predicted outcome variable
 #' @param digits the number of digits to round the result to, default is 2
-#' @return a list of average predicted probability
+#' @return a vector of average predicted probability
 #' @noRd
 
 get_avg_prob <- function(data, outcome, group, probs, digits = 2) {
-  avg_prob <- list()
+  avg_prob <- c()
   for (i in unique(data[, group])) {
     sub_data <- data[data[, group] == i, ]
-    avg_prob[[paste0("Avg_Predicted_Prob_", i)]] <-
-      round(mean(sub_data[, probs]), digits)
+    avg_prob <- c(avg_prob, round(mean(sub_data[, probs]), digits))
   }
   return(avg_prob)
+}
+
+#' Calculate the all metrics at once
+#' @param data Data frame containing the outcome, predicted outcome, and
+#' sensitive attribute
+#' @param outcome the name of the outcome variable, it must be binary
+#' @param group the name of the sensitive attribute
+#' @param probs the name of the predicted outcome variable
+#' @param cutoff the threshold for the predicted outcome, default is 0.5
+#' @param digits the number of digits to round the result to, default is 2
+#' @return a Data frame of all metrics
+
+get_all_metrics <- function(data, outcome, group, probs, cutoff = 0.5,
+                            digits = 2) {
+  all_metrics <- list()  # Use a list to store metrics temporarily
+  metrics_names <- c("TPR", "FPR", "PPR", "PPV", "NPV", "ACC", "Brier Score",
+                     "FN/FP Ratio", "Avg Pred Prob")
+
+  # Append each metric to the list
+  all_metrics[[1]] <- get_tpr(data, outcome, group, probs, cutoff, digits)
+  all_metrics[[2]] <- get_fpr(data, outcome, group, probs, cutoff, digits)
+  all_metrics[[3]] <- get_ppr(data, outcome, group, probs, cutoff, digits)
+  all_metrics[[4]] <- get_ppv(data, outcome, group, probs, cutoff, digits)
+  all_metrics[[5]] <- get_npv(data, outcome, group, probs, cutoff, digits)
+  all_metrics[[6]] <- get_acc(data, outcome, group, probs, cutoff, digits)
+  all_metrics[[7]] <- get_brier_score(data, outcome, group, probs, digits)
+  all_metrics[[8]] <- get_err_ratio(data, outcome, group, probs, cutoff, digits)
+  all_metrics[[9]] <- get_avg_prob(data, outcome, group, probs, digits)
+
+  # Convert the list of metrics into a data frame
+  all_metrics_df <- do.call(rbind, all_metrics)
+  rownames(all_metrics_df) <- metrics_names  # Assign metric names as row names
+
+  # Create column names based on unique groups
+  group_names <- paste0("Group ", sort(unique(data[, group])))
+  colnames(all_metrics_df) <- group_names
+
+  return(all_metrics_df)
 }
